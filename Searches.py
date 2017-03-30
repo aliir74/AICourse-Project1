13a4e263dfacd597f8a5cae465b33cd952e619c0
Ali Irani <aliirani74@gmail.com>
2017-03-30 20:33:17 +0430

    Initial commit

---
+++ Searches.py
@@ -0,0 +1,15 @@
+import sys
+def BFS(initialState, Actions, GoalTest):
+    f = [initialState]
+    ans = []
+    while(f != []):
+        currentNode = f[0]
+        del f[0]
+        if(GoalTest(currentNode)):
+            ans.append(currentNode)
+        f += Actions(currentNode)
+    print('F is empty!', sys.stderr)
+    return ans
+
+
+
