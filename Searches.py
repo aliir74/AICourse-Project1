e5b8f513505d57d7fca23d7b45b0bf4c583a6afe
Ali Irani <aliirani74@gmail.com>
2017-03-31 10:53:00 +0430

    add bsfTree classical search

--- Searches.py
+++ Searches.py
@@ -1,17 +1,15 @@
 import sys
-def BFS(initialState, Actions, GoalTest):
-    f = [initialState]
-    ans = []
+def BFSTree(initialState, Actions, GoalTest, Result, stepCost, pathCost):
+    f = [initialState()]
     while(f != []):
+        stepCost()
         currentNode = f[0]
         del f[0]
         if(GoalTest(currentNode)):
-            ans.append(currentNode)
-        f += Actions(currentNode)
+            return currentNode, pathCost()
+        actions = Actions(currentNode)
+        for a in actions:
+            f += Result(currentNode, a)
     print('F is empty!', sys.stderr)
-    return ans
-
-
-
 
 
