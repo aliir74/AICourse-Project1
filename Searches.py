6f133e19059fe8e58599517a619e4daddd627385
Ali Irani <aliirani74@gmail.com>
2017-03-31 11:40:54 +0430

    add DLS

--- Searches.py
+++ Searches.py
@@ -13,3 +13,27 @@ def BFSTree(initialState, Actions, GoalTest, Result, stepCost, pathCost):
     print('F is empty!', sys.stderr)
 
 
+def DLS(initialState, Actions, GoalTest, Result, stepCost, pathCost):
+    return DLSRecursive(initialState(), Actions, GoalTest, Result, stepCost, pathCost)
+
+def DLSRecursive(state, Actions, GoalTest, Result, stepCost, pathCost, limit):
+    if(GoalTest(state)):
+        return state, pathCost()
+    elif(limit == 0):
+        return 'cutoff', pathCost()
+
+    cutoff = False
+    for a in Actions(state):
+        child = Result(state, a)
+        stepCost()
+        res = DLSRecursive(state, Actions, GoalTest, Result, stepCost, pathCost, limit-1)
+        if(res == 'cutoff'):
+            cutoff = True
+        elif(res != 'failure'):
+            return res, pathCost()
+    if(cutoff):
+        return 'cutoff', pathCost()
+    else:
+        return 'failure', pathCost()
+
+
